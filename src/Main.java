24806d40c9f59969cd6c26fd777c469d91afa4cd
thom <thomas.henrique@aluno.ifsp.edu.br>
2023-06-27 19:19:30 -0300

    CRUD JAVA

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static void CREATE(File arquivo, String linha) {
+        try {
+            FileWriter escrita = new FileWriter(arquivo, true);
+            escrita.write(linha + '\n');
+
+            escrita.close();
+        } catch (IOException e ) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void READ(File arquivo, String[] linhas) {
+        try {
+            FileReader leitura = new FileReader(arquivo);
+            BufferedReader buff = new BufferedReader(leitura);
+
+            String linha_atual;
+            int contador_linhas = 0;
+
+            while((linha_atual = buff.readLine()) != null) {
+                linhas[contador_linhas] = linha_atual;
+                ++contador_linhas;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void UPDATE(File arquivo, String antiga, String nova, String[] linhas) {
+        try {
+            FileReader leitura = new FileReader(arquivo);
+            BufferedReader buff = new BufferedReader(leitura);
+
+            String linha_atual;
+            int contador_linhas = 0;
+
+            while((linha_atual = buff.readLine()) != null) {
+                if (linha_atual == antiga) {
+                    linha_atual = nova;
+                }
+                linhas[contador_linhas] = linha_atual;
+                ++contador_linhas;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void DELETE(File arquivo, String deletar, String[] linhas){
+        try {
+            FileReader leitura = new FileReader(arquivo);
+            BufferedReader buff = new BufferedReader(leitura);
+
+            String linha_atual;
+            int contador_linhas = 0;
+
+            while((linha_atual = buff.readLine()) != null) {
+                if (linha_atual != deletar) {
+                    linhas[contador_linhas] = linha_atual;
+                    ++contador_linhas;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
